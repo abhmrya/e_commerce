70de8ffa6440489028355c49d5a4c1b65e6c2414
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-06-11 02:26:04 +0530

    smpt off

--- e_commerce/authentication/signals.py
+++ e_commerce/authentication/signals.py
@@ -38,21 +38,21 @@ from django.contrib.auth.signals import user_logged_in
 
 
 # Signal for user registration
-@receiver(post_save, sender=User)
-def send_welcome_email_signal(sender, instance, created, **kwargs):
-    if created:
-        print('signal of registration send email')
-        subject = "Welcome to Our Site!"
-        message = f"Hello {instance.username}, thanks for registering!"
-        recipient_list = [instance.email]
-        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=False)
+# @receiver(post_save, sender=User)
+# def send_welcome_email_signal(sender, instance, created, **kwargs):
+#     if created:
+#         print('signal of registration send email')
+#         subject = "Welcome to Our Site!"
+#         message = f"Hello {instance.username}, thanks for registering!"
+#         recipient_list = [instance.email]
+#         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=False)
 
 
-# Signal for user login
-@receiver(user_logged_in)
-def show_login_info(sender, request, user, **kwargs):
-    print('signal of send email user login')
-    subject = "Welcome Back!"
-    message = f"Hello {user.username}, you have successfully logged in!"
-    recipient_list = [user.email]
-    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=False)
+# # Signal for user login
+# @receiver(user_logged_in)
+# def show_login_info(sender, request, user, **kwargs):
+#     print('signal of send email user login')
+#     subject = "Welcome Back!"
+#     message = f"Hello {user.username}, you have successfully logged in!"
+#     recipient_list = [user.email]
+#     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=False)
