71a5e74bbcea1763bec719d6f42b2638e4705066
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-06-11 01:24:59 +0530

    task delete

--- e_commerce/authentication/signals.py
+++ e_commerce/authentication/signals.py
@@ -1,9 +1,41 @@
+# from django.db.models.signals import post_save
+# from django.contrib.auth.models import User
+# from django.dispatch import receiver
+# from .tasks import send_welcome_email as send_welcome_email_task  
+# from django.core.mail import send_mail
+# from django.conf import settings
+
+# # Signal for user registration
+# @receiver(post_save, sender=User)
+# def send_welcome_email_signal(sender, instance, created, **kwargs):
+#     if created:
+#         print('signal of registration send email')
+#         subject = "Welcome to Our Site!"
+#         message = f"Hello {instance.username}, thanks for registering!"
+#         recipient_list = [instance.email]
+#         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=False)
+#         send_welcome_email_task.delay(subject, message, recipient_list)  
+
+# # Signal for user login
+# from django.contrib.auth.signals import user_logged_in
+
+# @receiver(user_logged_in)
+# def show_login_info(sender, request, user, **kwargs):
+#     print('signal of send email user login')
+#     subject = "Welcome Back!"
+#     message = f"Hello {user.username}, you have successfully logged in!"
+#     recipient_list = [user.email]
+#     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=False)
+
+#     send_welcome_email_task.delay(subject, message, recipient_list)
+
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .tasks import send_welcome_email as send_welcome_email_task  
 from django.core.mail import send_mail
 from django.conf import settings
+from django.contrib.auth.signals import user_logged_in
+
 
 # Signal for user registration
 @receiver(post_save, sender=User)
@@ -14,11 +46,9 @@ def send_welcome_email_signal(sender, instance, created, **kwargs):
         message = f"Hello {instance.username}, thanks for registering!"
         recipient_list = [instance.email]
         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=False)
-        send_welcome_email_task.delay(subject, message, recipient_list)  
+
 
 # Signal for user login
-from django.contrib.auth.signals import user_logged_in
-
 @receiver(user_logged_in)
 def show_login_info(sender, request, user, **kwargs):
     print('signal of send email user login')
@@ -26,5 +56,3 @@ def show_login_info(sender, request, user, **kwargs):
     message = f"Hello {user.username}, you have successfully logged in!"
     recipient_list = [user.email]
     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=False)
-
-    send_welcome_email_task.delay(subject, message, recipient_list)
--- e_commerce/authentication/tasks.py
+++ e_commerce/authentication/tasks.py
@@ -1,12 +1,25 @@
-from celery import shared_task
+# from celery import shared_task
+# from django.core.mail import send_mail
+# from django.conf import settings
+
+# @shared_task
+# def send_welcome_email(subject, message, recipient_list):
+#     print(f"[TASK START] Sending email to {recipient_list}")
+#     try:
+#         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=False)
+#         print("[TASK SUCCESS] Email sent.")
+#     except Exception as e:
+#         print(f"[TASK ERROR] {e}")
+
 from django.core.mail import send_mail
+from django.contrib.auth.models import User
 from django.conf import settings
 
-@shared_task
-def send_welcome_email(subject, message, recipient_list):
-    print(f"[TASK START] Sending email to {recipient_list}")
-    try:
-        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=False)
-        print("[TASK SUCCESS] Email sent.")
-    except Exception as e:
-        print(f"[TASK ERROR] {e}")
+def send_welcome_email(user_id):
+    user = User.objects.get(id=user_id)
+    send_mail(
+        subject="Welcome to MyShop!",
+        message=f"Hi {user.username}, welcome aboard!",
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[user.email],
+    )
